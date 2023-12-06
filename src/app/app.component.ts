efa52181926800b59a89ea78aa8e2e7f1c7fbfb3
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2023-12-06 14:59:46 -0400

    Initial implementatio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+
+export interface PeriodicElement {
+  todo: string;
+  position: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, todo: 'Apples'},
+  { position: 2, todo: 'Bread'},
+  { position: 3, todo: 'Sauce'},
+  { position: 4, todo: 'Eggs'},
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatTableModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todos-mfe';
+  displayedColumns: string[] = ['position', 'todo'];
+  dataSource = ELEMENT_DATA;
 }
